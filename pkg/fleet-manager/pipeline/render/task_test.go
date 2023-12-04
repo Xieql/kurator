1b9a2b3f5259598c3de2712ed2957123b080e4b3
Xieql <xieqianglong@huawei.com>
2023-12-04 19:22:33 +0800

    pipeline: add renderTask func
    Signed-off-by: Xieql <xieqianglong@huawei.com>

---
+++ pkg/fleet-manager/pipeline/render/task_test.go
@@ -0,0 +1,108 @@
+/*
+Copyright Kurator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package render
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"kurator.dev/kurator/pkg/fleet-manager/manifests"
+)
+
+var manifestFS = manifests.BuiltinOrDir("manifests/tasks")
+
+const expectedTaskFilePath = "testdata/tasks/"
+
+func TestRenderTask(t *testing.T) {
+	// Define test cases for various task templates and configurations.
+	cases := []struct {
+		name         string
+		cfg          TaskConfig
+		expectError  bool
+		expectedFile string
+	}{
+		// ---- Case: Default Configuration for Git Clone ----
+		// This case tests the basic configuration of the 'git-clone' template.
+		// It will not include auth, because auth will add in pipeline.
+		{
+			name: "git-clone with basic parameters",
+			cfg: TaskConfig{
+				PipelineNamespace: "kurator-pipeline",
+				TaskType:          "git-clone",
+				Params:            map[string]string{},
+			},
+			expectError:  false,
+			expectedFile: "git-clone.yaml",
+		},
+
+		// ---- Case: Default Configuration for Go Test ----
+		// This case tests the default configuration of the 'go-test' template.
+		// It uses the default namespace and relies on all default parameter values.
+		{
+			name: "go-test with default parameters",
+			cfg: TaskConfig{
+				PipelineNamespace: "default",
+				TaskType:          "go-test",
+				Params:            map[string]string{},
+			},
+			expectError:  false,
+			expectedFile: "go-test-default.yaml",
+		},
+
+		// ---- Case: Custom Configuration for Go Test ----
+		// This case customizes the 'go-test' template: setting Go version to 1.20,
+		// targeting the './pkg/...' package path, and specifying the Linux ARM architecture.
+		{
+			name: "go-test with custom parameters - Go 1.20, ./pkg/..., Linux ARM",
+			cfg: TaskConfig{
+				PipelineNamespace: "kurator-pipeline",
+				TaskType:          "go-test",
+				Params: map[string]string{
+					"packages": "./pkg/...",
+					"version":  "1.20",
+					"GOOS":     "linux",
+					"GOARCH":   "arm",
+				},
+			},
+			expectError:  false,
+			expectedFile: "go-test-custom-value.yaml",
+		},
+
+		// TODO: Add more test cases here for different task templates or configurations...
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			fs := manifestFS
+
+			result, err := renderTask(fs, tc.cfg)
+
+			// Test assertions
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+
+				expected, err := os.ReadFile(expectedTaskFilePath + tc.expectedFile)
+				assert.NoError(t, err)
+				assert.Equal(t, string(expected), string(result))
+			}
+		})
+	}
+}
