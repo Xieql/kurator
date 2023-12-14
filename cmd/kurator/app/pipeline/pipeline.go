61fc5873922cfaa9f1b50081687154bbb3aa54e9
Xieql <xieqianglong@huawei.com>
2023-12-14 15:04:15 +0800

    fix
    Signed-off-by: Xieql <xieqianglong@huawei.com>

---
+++ cmd/kurator/app/pipeline/pipeline.go
@@ -0,0 +1,51 @@
+/*
+Copyright Kurator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pipeline
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"kurator.dev/kurator/pkg/version"
+)
+
+func NewCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "pipeline",
+		Short: "Print the info of kurator pipeline",
+		Run: func(cmd *cobra.Command, args []string) {
+			_ = RunVersion(cmd)
+		},
+	}
+	return cmd
+}
+
+// RunVersion provides the version information of keadm in format depending on arguments
+// specified in cobra.Command.
+func RunVersion(cmd *cobra.Command) error {
+	v := version.Get()
+
+	y, err := json.MarshalIndent(&v, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(y))
+
+	return nil
+}
